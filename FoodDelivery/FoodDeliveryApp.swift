b62a7fa1bc829fe84d22ad3b6597a3b2a6a00864
sxc <qiuyequ@icloud.com>
2020-09-22 14:35:20 +0800

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by Xiaochun Shen on 2020/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
